d612280206f85ba6e1b99507ab7b9e81c8329ebf
avila <avila@sonicon.net>
2014-03-04 18:16:37 +0100

    Correct documentation url

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -3,7 +3,7 @@
     <div id="content">
         
         <h1>Welcome to PIP</h1>
-        <p>To get started please read the documentation at <a href="http://pip.dev7studios.com/">http://pip.dev7studios.com</a>.</p>
+        <p>To get started please read the documentation at <a href="http://gilbitron.github.io/PIP/">http://gilbitron.github.io/PIP/</a>.</p>
         
     </div>
 
