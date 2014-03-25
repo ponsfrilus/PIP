cf78edf7f63f21a277f0cf71505243d3f1f6a0bb
Sam Izzo <sam@humbug.net>
2014-03-26 01:47:26 +1100

    * Added ability to specify a pre-routing handler which can forcibly redirect to a specific controller and action

--- system/pip.php
+++ system/pip.php
@@ -15,28 +15,43 @@ function controllerExists($controller)
 function pip()
 {
     global $config;
-    
-    // Set our defaults
-    $controller = $config['default_controller'];
-    $action = 'index';
-    $url = '';
-    
-    // Get request url and script url
-    $request_url = (isset($_SERVER['REQUEST_URI'])) ? $_SERVER['REQUEST_URI'] : '';
-    $script_url  = (isset($_SERVER['PHP_SELF'])) ? $_SERVER['PHP_SELF'] : '';
+
+    if (isset($config['pre_route_handler']) && is_string($config['pre_route_handler']))
+    {
+        $override = call_user_func($config['pre_route_handler']);
+    }
+
+    $segments = array();
+
+    if (isset($override) && isset($override['controller']) && isset($override['action']))
+    {
+        $controller = $override['controller'];
+        $action = $override['action'];
+    }
+    else
+    {
+        // Set our defaults
+        $controller = $config['default_controller'];
+        $action = 'index';
+        $url = '';
         
-    // Get our url path and trim the / of the left and the right
-    if($request_url != $script_url) $url = trim(preg_replace('/'. str_replace('/', '\/', str_replace('index.php', '', $script_url)) .'/', '', $request_url, 1), '/');
-    
-    // Split the url into segments
-    $segments = explode('/', $url);
-    
-    // Do our default checks
-    $hasAction = false;
-    if(isset($segments[0]) && $segments[0] != '') $controller = $segments[0];
-    if(isset($segments[1]) && $segments[1] != '') {
-        $action = $segments[1];
-        $hasAction = true;
+        // Get request url and script url
+        $request_url = (isset($_SERVER['REQUEST_URI'])) ? $_SERVER['REQUEST_URI'] : '';
+        $script_url  = (isset($_SERVER['PHP_SELF'])) ? $_SERVER['PHP_SELF'] : '';
+            
+        // Get our url path and trim the / of the left and the right
+        if($request_url != $script_url) $url = trim(preg_replace('/'. str_replace('/', '\/', str_replace('index.php', '', $script_url)) .'/', '', $request_url, 1), '/');
+        
+        // Split the url into segments
+        $segments = explode('/', $url);
+        
+        // Do our default checks
+        $hasAction = false;
+        if(isset($segments[0]) && $segments[0] != '') $controller = $segments[0];
+        if(isset($segments[1]) && $segments[1] != '') {
+            $action = $segments[1];
+            $hasAction = true;
+        }
     }
 
     if (controllerExists($controller)) {
