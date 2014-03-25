8ff50c66d7d9a689b83a27a7feca2fb9eb8510cd
Sam Izzo <sam@humbug.net>
2014-03-26 00:47:15 +1100

    * View set method can now take an array of vars to set

--- system/view.php
+++ system/view.php
@@ -10,9 +10,16 @@ class View {
 		$this->template = APP_DIR .'views/'. $template .'.php';
 	}
 
-	public function set($var, $val)
+	public function set($var, $val = null)
 	{
-		$this->pageVars[$var] = $val;
+		if (is_array($var))
+		{
+			$this->pageVars = array_merge($this->pageVars, $var);
+		}
+		else if (isset($val))
+		{
+			$this->pageVars[$var] = $val;
+		}
 	}
 
 	public function render()
